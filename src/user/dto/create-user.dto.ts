9df60f3a4bcef92807871c68fd5df7dd22bc0ab4
HaoYuZhang <zhanghy2170@chinaunicom.cn>
2023-08-31 16:10:26 +0800

    P1-P4

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  /**
+   * 手机号（系统唯一）
+   */
+  @ApiProperty({ example: '13611177421' })
+  readonly phoneNumber: string;
+
+  @ApiProperty({ example: '然叔' }) name: string;
+
+  @ApiProperty({ example: '123456' })
+  password: string;
+
+  @ApiProperty({ example: '15906475@qq.com' })
+  email: string;
+}
